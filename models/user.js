efbac32ca4e96cfed99ceb1b09c3b8b95fad6813
roberto cortes <robertcortesgrimm223@gmail.com>
2020-01-24 16:41:21 -0600

    user model created

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	dob: {
+		type: Date,
+		required: true
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now()
+	},
+	profilePhoto: {
+		type: String,
+		default: ''
+	}
+	author: {
+		type: Boolean,
+		default: false
+	}
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
