bb21a5fa2fc76f585c01cde3605778b2c64787fc
roberto cortes <robertcortesgrimm223@gmail.com>
2020-01-25 16:18:11 -0600

    user edit,show,index rendered

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,27 +4,42 @@ const router = express.Router()
 const User = require('../models/user.js')
 
 
-router.get('/', (req, res) => {
+router.get('/profile', (req, res) => {
 	res.render('user/show.ejs')
 })
 
 
 
+router.get('/', async (req,res,next) => {
+	try {
+
+		// here we will render a list of all authors on the site not including 
+		// the person logged in
+
+
+		res.render('user/index.ejs')
+
+	}catch(err){
+		next(err)
+	}
+
+	})
 
 
 
+router.get('/profile/:id/edit', async (req,res,next) => {
+	try {
+		// from the profile page the user can select edit profile
+		// to route them to a edit profile page, this will include
+		// all of their information
 
+		res.render('user/edit.ejs')
 
+	}catch(err){
+		next(err)
+	}
 
-
-
-
-
-
-
-
-
-
+	})
 
 
 
