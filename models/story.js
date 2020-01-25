eb30757ba623bf057143117d2a8c0f94a0d2ff35
roberto cortes <robertcortesgrimm223@gmail.com>
2020-01-25 13:44:16 -0600

    story model created

---
+++ models/story.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const storySchema = mongoose.Schema({
+
+	title: {
+		type: String,
+		required: true
+	},
+	genre: {
+		type: String,
+		required: true
+	},
+	body: {
+		type: String,
+		required: true
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now()
+	}
+
+})
+
+const Story = mongoose.model('Story', storySchema)
+
+module.exports = Story
